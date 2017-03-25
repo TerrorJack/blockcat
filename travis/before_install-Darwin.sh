386c5fff6e06d3546c59af1a5f6d3a240b7b4ca8
Shao Cheng <astrohavoc@gmail.com>
2017-03-25 23:07:31 +0800

    Add proper caching back.

--- travis/before_install-Darwin.sh
+++ travis/before_install-Darwin.sh
@@ -5,16 +5,17 @@ sudo brew upgrade
 sudo brew install ninja
 
 cd $TRAVIS_BUILD_DIR
-travis_retry curl -L https://github.com/WebAssembly/binaryen/archive/master.zip -o binaryen-master.zip
-unzip -q binaryen-master.zip
-cd binaryen-master
+if [ ! -d "binaryen/.git" ]; then
+  rm -rf binaryen
+  git clone --depth 1 https://github.com/WebAssembly/binaryen.git
+fi
+cd binaryen
+git pull
 cmake -DCMAKE_C_COMPILER=`which clang` -DCMAKE_CXX_COMPILER=`which clang++` -DCMAKE_INSTALL_PREFIX=~/.local -G Ninja .
-ninja
 ninja install
 
 cd $TRAVIS_BUILD_DIR
-export PATH=~/.local/bin:/opt/ghc/8.0.2/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
+curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
 stack config set system-ghc --global true
 stack config set install-ghc --global true
 stack --no-terminal build --test --bench --only-dependencies
--- travis/before_install-Linux.sh
+++ travis/before_install-Linux.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 cd $TRAVIS_BUILD_DIR
-travis_retry curl -L https://github.com/WebAssembly/binaryen/archive/master.zip -o binaryen-master.zip
-unzip -q binaryen-master.zip
-cd binaryen-master
+if [ ! -d "binaryen/.git" ]; then
+  rm -rf binaryen
+  git clone --depth 1 https://github.com/WebAssembly/binaryen.git
+fi
+cd binaryen
+git pull
 cmake -DCMAKE_C_COMPILER=`which clang-5.0` -DCMAKE_CXX_COMPILER=`which clang++-5.0` -DCMAKE_INSTALL_PREFIX=~/.local -G Ninja .
-ninja
 ninja install
 
 cd $TRAVIS_BUILD_DIR
-export PATH=~/.local/bin:/opt/ghc/8.0.2/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 stack config set system-ghc --global true
 stack config set install-ghc --global true
 stack --no-terminal build --test --bench --only-dependencies
